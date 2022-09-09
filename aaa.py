efdf88a5e2b65eff01c2ea5989d229536e5f1635
Hillary Chan <44228565+hillaryychan@users.noreply.github.com>
2022-09-09 11:30:13 +1000

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -9,3 +9,27 @@ def is_isbn(number, *args, **kwargs):
         val += i * num
 
     return val % 11 == 0
+
+def is_isbn(number, *args, **kwargs):
+    number = get_isbn_digits(number)
+    # Must have 10 digits
+    if len(number) != 10:
+        return False
+
+    val = 0
+    for i, num in enumerate(number, 1):
+        val += i * num
+
+    return val % 11 == 0
+
+def is_isbn(number, *args, **kwargs):
+    number = get_isbn_digits(number)
+    # Must have 10 digits
+    if len(number) != 10:
+        return False
+
+    val = 0
+    for i, num in enumerate(number, 1):
+        val += i * num
+
+    return val % 11 == 0
